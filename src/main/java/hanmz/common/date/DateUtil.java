a1b34b7b93de6d7f7b349abafdd011c48490ebf1
hanmz <hanmz@fxiaoke.com>
2016-08-21 12:31:45 +0800

    generate structure

---
+++ src/main/java/hanmz/common/date/DateUtil.java
@@ -0,0 +1,7 @@
+package hanmz.common.date;
+
+/**
+ * Created by hanmz on 2016/8/21.
+ */
+public class DateUtil {
+}
---
+++ src/main/java/hanmz/common/traversal/TraUtil.java
@@ -0,0 +1,7 @@
+package hanmz.common.traversal;
+
+/**
+ * Created by hanmz on 2016/8/21.
+ */
+public class TraUtil {
+}
